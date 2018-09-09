155e020b5718f60969cebaf3110f83e073adc108
Bob Reselman <bob@CogArtTech.com>
2018-09-08 20:03:36 -0700

    Update launcher-on-3000.sh
    build out the deployment and service launch script

--- simpleapi/launcher-on-3000.sh
+++ simpleapi/launcher-on-3000.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+#use the minikube Docker daemon
+eval $(minikube docker-env)builf
+#build the Docker image
+docker build -t simpleapi:v1 .
+#create the deployment
+kubectl run simpleapi --image=simpleapi:v1 --port=3000 --image-pull-policy=Never
+#fire up the deployment
+kubectl expose deployment simpleapi --type=LoadBalancer
+#try to load the service
+minikube service simpleapi
