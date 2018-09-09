6112c838c6b336013311b88ea61a29642ed4d310
Bob Reselman <bob@CogArtTech.com>
2018-09-08 20:08:22 -0700

    Built out launch script
    Built out launch script

--- readerwriter/launcher-on-8111.sh
+++ readerwriter/launcher-on-8111.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/env bash
+#use the minikube Docker daemon
+eval $(minikube docker-env)builf
+#build the Docker image
+docker build -t readerwriter:v1 .
+#create the deployment
+kubectl run readerwriter --image=readerwriter:v1 --port=8111 --image-pull-policy=Never
+#fire up the deployment
+kubectl expose deployment readerwriter --type=LoadBalancer
+#try to load the service
+minikube service readerwriter
