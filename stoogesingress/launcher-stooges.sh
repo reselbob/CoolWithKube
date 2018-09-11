a84a3bc048cb30b59f4227ca222d0cc9e71a6925
Bob Reselman <reselbob@gmail.com>
2018-09-11 12:21:42 -0700

    Making everything a string in values.yaml

--- stoogesingress/launcher-stooges.sh
+++ stoogesingress/launcher-stooges.sh
@@ -4,5 +4,11 @@ eval $(minikube docker-env)build
 #build the Docker image
 docker build -t moe:v1 .
 docker build -t larry:v1 .
-docker build -t currly:v1 .
+docker build -t curly:v1 .
+
+#service run
+
+kubectl expose deployment moe --target-port=8130 --type=NodePort
+kubectl expose deployment larry --target-port=8130 --type=NodePort
+kubectl expose deployment curly --target-port=8130 --type=NodePort
 
