d066b65e48695efb652285c56b4affdc4dfb6b76
Bob Reselman <bob@CogArtTech.com>
2018-09-19 20:55:20 -0700

    upped the replicas

--- Day-1/readerwriter/launcher-on-8111.sh
+++ Day-1/readerwriter/launcher-on-8111.sh
@@ -4,7 +4,7 @@ eval $(minikube docker-env)build
 #build the Docker image
 docker build -t readerwriter:v1 .
 #create the deployment
-kubectl run readerwriter --image=readerwriter:v1 --port=8111 --image-pull-policy=Never
+kubectl run readerwriter --image=readerwriter:v1 --port=8111 --image-pull-policy=Never --replicas=3
 #fire up the deployment
 kubectl expose deployment readerwriter --type=LoadBalancer
 #try to load the service
