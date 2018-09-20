d0e1d4174cdafa50a2dd386dfb75e944bcff67c0
Bob Reselman <reselbob@gmail.com>
2018-09-19 17:36:47 -0700

    Made it a nodeport

--- Day-1/simpleapi_v1/launcher-on-8113.sh
+++ Day-1/simpleapi_v1/launcher-on-8113.sh
@@ -6,6 +6,6 @@ docker build -t simpleapi:v1 .
 #create the deployment
 kubectl run simpleapi --image=simpleapi:v1 --port=8113 --image-pull-policy=Never
 #fire up the deployment
-kubectl expose deployment simpleapi --type=LoadBalancer
+kubectl expose deployment simpleapi --type=NodePort
 #try to load the service
 minikube service simpleapi
