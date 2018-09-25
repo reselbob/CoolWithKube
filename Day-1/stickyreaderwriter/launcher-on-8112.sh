77e7fea212ce841bfedddf5fa258eafb454e067e
Bob Reselman <reselbob@gmail.com>
2018-09-25 14:11:59 -0700

    fixed typo

--- Day-1/stickyreaderwriter/launcher-on-8112.sh
+++ Day-1/stickyreaderwriter/launcher-on-8112.sh
@@ -4,15 +4,15 @@
 #Go back to kubectl command line
 
 #Build the persistent volume
-kubectl appy -f sticky-pv.yaml
+kubectl apply -f sticky-pv.yaml
 #Build the persistent volume claim
-kubectl appy -f sticky-pvc.yaml
+kubectl apply -f sticky-pvc.yaml
 #use the minikube Docker daemon
 eval $(minikube docker-env)build
 #build the Docker image
 docker build -t stickyreaderwriter:v1 .
 #create the pod
-kubectl appy -f sticky-pod.yaml
+kubectl apply -f sticky-pod.yaml
 #get into the pod/container
 kubectl exec -it stickyreaderwriter -- /bin/bash
 #in the container
