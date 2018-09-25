334208d1b7becc1895016ad8aab597fca8398830
Bob Reselman <reselbob@gmail.com>
2018-09-25 14:29:47 -0700

    resetting stickyreaderwriter to heavy node:8

--- Day-1/stickyreaderwriter/launcher-on-8112.sh
+++ Day-1/stickyreaderwriter/launcher-on-8112.sh
@@ -12,7 +12,11 @@ eval $(minikube docker-env)build
 #build the Docker image
 docker build -t stickyreaderwriter:v1 .
 #create the pod
-kubectl apply -f sticky-pod.yaml
+#kubectl apply -f sticky-pod.yaml
+#create the deployment
+kubectl run stickyreaderwriter --image=stickyreaderwriter:v1 --port=8112 --image-pull-policy=Never --replicas=3
+#create the service
+kubectl expose deployment stickyreaderwriter --type=NodePort
 #get into the pod/container
 kubectl exec -it stickyreaderwriter -- /bin/bash
 #in the container
