32969cec280f05aa205177500e33f79b3ae77b2c
Bob Reselman <reselbob@gmail.com>
2018-09-26 14:20:55 -0700

    added review lab instructions

--- Day-2/reviewlabone/launcher-review-lab-one.sh
+++ Day-2/reviewlabone/launcher-review-lab-one.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd app
 #use the minikube Docker daemon
 eval $(minikube docker-env)build
 #build the Docker image
@@ -7,4 +8,6 @@ docker build -t reviewlabone:v1 .
 #create the deployment
 
 #create the service
+
+#prove it all works
 
