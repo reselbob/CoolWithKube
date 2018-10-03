ed3d71256bf9866953e0ed514dc88dddde752578
Bob Reselman <reselbob@gmail.com>
2018-10-03 13:59:53 -0700

    Adding an async version, just to do the right thing

---
+++ Day-1/readerwriter/async-app/server.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const fs = require('fs');
+const {promisify} = require('util');
+const readFileAsync = promisify(fs.readFile);
+const appendFileAsync = promisify(fs.appendFile);
+let i = 0;
+//Create an Async version of the request handler. It really is the right thing to do.
+const handleRequest = function (request, response) {
+    const res = response;
+    const fname = 'test_file.txt';
+    const str = i++ + ' at ' + new Date() + '\n';
+    appendFileAsync(fname, str)
+        .then((result) => {
+            return readFileAsync(fname, {encoding: 'utf8'});
+        })
+        .then(result => {
+            res.statusCode = 200;
+            res.end(result);
+        })
+        .catch(err => {
+            res.statusCode = 400;
+            res.end(err);
+        });
+};
+const www = http.createServer(handleRequest);
+www.listen(8111, () => {
+    console.log('Listening on port 8111');
+});
