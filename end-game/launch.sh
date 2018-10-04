c1a9f0b1181be3a649e4906e2e20e92f2ef33c10
Bob Reselman <bob@CogArtTech.com>
2018-10-03 22:26:04 -0700

    added choice for repo

--- end-game/launch.sh
+++ end-game/launch.sh
@@ -3,3 +3,6 @@
 eval $(minikube docker-env)build
 #build the Docker image
 docker build -t feelingtracker:v1 . # !/usr/bin/env bash
+
+#from docker
+docker build -t reselbob/feelingtracker:latest . # !/usr/bin/env bash
