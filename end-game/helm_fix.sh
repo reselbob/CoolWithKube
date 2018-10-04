d34a38e6cc0e92a70cbd72f51c15e62462145948
Bob Reselman <reselbob@gmail.com>
2018-10-03 23:03:11 -0700

    fixed file extension typo again

--- end-game/helm_fix.sh
+++ end-game/helm_fix.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
@@ -5,5 +6,5 @@ helm init
 
 #get the name
 helm list
-#.... and thn
+#.... and then
 helm delete THE_NANME
