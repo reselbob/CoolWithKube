17f778534564e6af4cb65b1fd2c6f5bd6dbb680e
Bob Reselman <bob@CogArtTech.com>
2018-10-03 22:45:50 -0700

    added instuctions for helm

--- end-game/helm_fix.hs
+++ end-game/helm_fix.hs
@@ -2,3 +2,8 @@ kubectl create serviceaccount --namespace kube-system tiller
 kubectl create clusterrolebinding tiller-cluster-rule --clusterrole=cluster-admin --serviceaccount=kube-system:tiller
 kubectl patch deploy --namespace kube-system tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccount":"tiller"}}}}'
 helm init
+
+#get the name
+helm list
+#.... and thn
+helm delete THE_NANME
