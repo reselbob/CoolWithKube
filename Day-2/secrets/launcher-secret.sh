a82e2f898c04dc270041ec2b64c39024956fdd35
Bob Reselman <bob@CogArtTech.com>
2018-10-09 12:25:38 -0700

    fixed typo

--- Day-2/secrets/launcher-secret.sh
+++ Day-2/secrets/launcher-secret.sh
@@ -2,7 +2,7 @@
 #Create secrets at the command line
 kubectl create secret generic mysecrets --from-literal='A_SECRET=apples-taste-great' --from-literal='B_SECRET=the-bee-gees-rock'
 #create a secret from file. The key will be the file name
-kubectl create secret generic yoursecrets  --from-file=./secrets_files/X_SECRET
+kubectl create secret generic yoursecrets  --from-file=./secret_files/X_SECRET
 
 #take a look at the environment variables
 kubectl exec -ti simple-pod env
