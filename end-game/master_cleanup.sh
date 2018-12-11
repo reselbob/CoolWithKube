806c696840b5899b48369355c217047886828738
Bob Reselman <bob@CogArtTech.com>
2018-12-11 13:42:01 -0800

    initial commit

---
+++ end-game/master_cleanup.sh
@@ -0,0 +1,7 @@
+subo kubeadm reset
+sudo apt-get purge kubeadm kubectl kubelet kubernetes-cni kube*
+sudo apt-get autoremove
+sudo rm -f /home/ubuntu/.kube/config
+sudo rm -rf  /var/lib/etcd/member/snap
+sudo rm -rf  /var/lib/etcd/member/wal
+
