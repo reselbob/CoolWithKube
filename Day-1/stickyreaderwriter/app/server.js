90c096cef8378da755755c4c4af1dbbe4fc7570b
Bob Reselman <reselbob@gmail.com>
2019-04-24 13:18:59 -0700

    Made the port definition subject to an process.env, but the default is 8112

--- Day-1/stickyreaderwriter/app/server.js
+++ Day-1/stickyreaderwriter/app/server.js
@@ -2,6 +2,8 @@ const http = require('http');
 const fs = require('fs');
 let i = 0;
 
+const port = process.env.PORT || 8112;
+
 const handleRequest = function(request, response) {
     const fname = '/mydata/test_file.txt';
     const str = `${i} as ${new Date()} for processId: ${process.pid} \n`;
@@ -17,6 +19,6 @@ const handleRequest = function(request, response) {
     response.end(contents);
 }
 const www = http.createServer(handleRequest);
-www.listen(8112, ()=>{
-    console.log('Listening on port 8112');
+www.listen(port, ()=>{
+    console.log(`Listening on port ${port}`);
 });
